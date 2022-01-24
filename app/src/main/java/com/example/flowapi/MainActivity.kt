9d603d09895db8ace001e49a13a2084a21c93c5c
vrajendraBhavsar <vrajendra.bhavsar@mindinventory.com>
2022-01-24 18:19:25 +0530

    asFlow Builder

--- app/src/main/java/com/example/flowapi/MainActivity.kt
+++ app/src/main/java/com/example/flowapi/MainActivity.kt
@@ -7,13 +7,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
-    //flowO builer ->  To Store list of data
+    //asFlow builder ->  To Convert list of data into Flow
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data = flowOf("vraj", "Riti", "Bruno", "Zayn", "Ranbir", "dualipa")
+        val data = listOf("vraj", "Riti", "Bruno", "Zayn", "Ranbir", "dualipa", "Niha").asFlow()
             .flowOn(Dispatchers.IO) //process background ma karava
 
         runBlocking {
