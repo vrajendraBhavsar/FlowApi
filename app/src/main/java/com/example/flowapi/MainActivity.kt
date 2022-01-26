f24c6fa0a2efb4a1b9205a7f267a9cdd533a6aa5
vrajendraBhavsar <vrajendra.bhavsar@mindinventory.com>
2022-01-26 12:55:39 +0530

    Used lifecycle scope instead of runBlocking

--- app/src/main/java/com/example/flowapi/MainActivity.kt
+++ app/src/main/java/com/example/flowapi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.flowapi
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
@@ -20,7 +21,16 @@ class MainActivity : AppCompatActivity() {
         val data = flowOf(1,2,3,4,5,6,7,8,9,10,11,12,13,43,65,75,99,122)
             .flowOn(Dispatchers.IO) //process background ma karava
 
-        runBlocking {
+        lifecycleScope.launchWhenStarted {
+            data
+                .filter {
+                    it%2 != 0   //odd
+                }
+                .collect{
+                    Log.d("VRAJTEST", "it%2 != 0 => $it")
+                }
+        }
+        /*runBlocking {
             data
                 .filter { value->
                     value%2 == 0    // Based on the condition you put here..data will get collected or ignored if it doesn't satisfy the condition
@@ -28,6 +38,6 @@ class MainActivity : AppCompatActivity() {
                 .collect {
                 Log.d("VRAJTEST", "Name of the person is $it")
             }
-        }
+        }*/
     }
 }
